ea9b5b406ccdf1421de3ea9b4e3db726a33a425d
Fedor24 <kurakin.fed@mail.ru>
2024-12-20 02:57:40 +0300

    Последние правки,реализована навигация по окнам у админа

--- Kurkain/Views/AdminWindow.xaml.cs
+++ Kurkain/Views/AdminWindow.xaml.cs
@@ -23,6 +23,29 @@ namespace Kurkain.Views
         {
             InitializeComponent();
         }
+        // Навигация для админа
+        public void EmployeeButton_Click(object sender, RoutedEventArgs e) 
+        { 
+            EmployeeManagement employeeManagement = new EmployeeManagement();
+            employeeManagement.Show();
+            this.Close();
+
+        }
+        public void OrderButton_Click(object sender, RoutedEventArgs e) 
+        { 
+            OrdersManagement ordersManagement = new OrdersManagement();
+            ordersManagement.Show();
+            this.Close();
+        }
+        public void ShiftManagement_Click(object sender, RoutedEventArgs e)
+        {
+            ShiftsManagement shiftManagement = new ShiftsManagement();
+            shiftManagement.Show();
+        }
+        public void ExitButton_Click(object sender, RoutedEventArgs e) 
+        { 
+            this.Close();
+        }
 
     }
 }
